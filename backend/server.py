f14cd3a8e12e913a41aea6c3a6c0aee25e872aa3
Diego Fernando Ristau <diegof.ristau@gmail.com>
2021-02-15 22:32:58 -0300

    Update home page layout and add cross-site settings.

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 from uuid import uuid4
+
+origins = ['http://localhost:3000']
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class RoomBody(BaseModel):
     nickname: str
-
 
 @app.post("/rooms")
 def get_rooms(body: RoomBody):
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,23 +1,125 @@
-import './styles.css'
-import { useHistory } from "react-router-dom"
+import "./styles.css";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useMemo, useState } from "react";
+
+const Gear = () => {
+  return (
+    <img
+      className="gear"
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Gear_icon_svg.svg"
+      alt="gear"
+    />
+  );
+};
+
+interface BotaoProps {
+  onClique: () => void;
+  loading: boolean;
+  textoDefault: string;
+  textoLoading: string;
+  disabled: boolean;
+}
+
+const Botao = ({ onClique, loading, textoDefault, textoLoading, disabled }: BotaoProps) => {
+  return (
+    <button onClick={onClique} disabled={disabled}>
+      {!loading && textoDefault}
+      {loading && (
+        <>
+          <span>{textoLoading}</span>
+          <Gear />
+        </>
+      )}
+    </button>
+  );
+};
+
+const ERROR_NICK_MESSAGE = "É necessário informar um nickname.";
+
+interface AlertProps {
+  message: string;
+}
+
+const Alert = ({ message }: AlertProps) => {
+  return (
+    <div className="alerta">
+      <span>{message}</span>
+    </div>
+  );
+};
 
 function Home() {
-
+  const [name, setName] = useState("");
   const history = useHistory();
+  const [fetching, setFetching] = useState<{ [key: string]: boolean }>({});
+  const [roomId, setRoomId] = useState("");
+  const [errorList, setErrorList] = useState<string[]>([]);
 
   const handlerTipoJogo = (tela: string) => {
-    const salaId = tela
-    history.push(`/room/${salaId}`)
-  }
+    // const salaId = tela
+
+    if (!name) {
+      setErrorList([ERROR_NICK_MESSAGE]);
+      return;
+    }
+
+    // verifica o tipo de pesquisa
+    // let fetching2 = fetching
+    // fetching2[tela] = true
+    // setFetching(fetching2);
+    setFetching({ ...fetching, [tela]: true });
+
+    axios
+      .post("http://localhost:8000/room", {
+        nickname: name,
+      })
+      .then((response) => {
+        setRoomId(response.data.id);
+        history.push(`/room/${response.data.id}`);
+      })
+      .catch((error) => {
+        setErrorList(["Erro ao criar a sala."]);
+      });
+  };
+
+  const errorNick = useMemo(
+    () => errorList.find((errorMessage) => errorMessage === ERROR_NICK_MESSAGE),
+    [errorList]
+  );
 
   return (
     <div className="home">
-        <div>
-            <h1>Battlefield</h1>
-            <input type="text" placeholder="Insert your nickname" />
-            <button onClick={() => handlerTipoJogo('privada')}>Partida privada</button>
-            <button onClick={() => handlerTipoJogo('procurar')}>Procurar jogador</button>
-        </div>
+      <div>
+        {errorList.map((item) => (
+          <Alert message={item} key={item} />
+        ))}
+        <h1>Exa Battleship</h1>
+        <input
+          type="text"
+          className={errorNick ? "erro" : ""}
+          placeholder="Insert your nickname"
+          onChange={(event) => {
+            setName(event.target.value);
+            setErrorList([]);
+          }}
+        />
+
+        <Botao
+          onClique={() => handlerTipoJogo("privada")}
+          loading={fetching["privada"] && !roomId}
+          textoDefault="Partida Privada"
+          textoLoading="Criando sala"
+          disabled={Object.keys(fetching).length !== 0}
+        />
+        <Botao
+          onClique={() => handlerTipoJogo("publica")}
+          loading={fetching["publica"] && !roomId}
+          textoDefault="Partida Pública"
+          textoLoading="Buscando sala"
+          disabled={Object.keys(fetching).length !== 0}
+        />
+      </div>
     </div>
   );
 }
